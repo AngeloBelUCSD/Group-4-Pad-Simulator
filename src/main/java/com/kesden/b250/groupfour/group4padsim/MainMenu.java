836aa985c299fe97e6f380dd250320b6c383bf8e
siotle <dimensionstraveler@gmail.com>
2015-11-03 19:33:47 -0800

    Added the most bland home screen ever with 3/4 nonfunctional buttons! Also set it as the default activity on startup.

---
+++ src/main/java/com/kesden/b250/groupfour/group4padsim/MainMenu.java
@@ -0,0 +1,23 @@
+package com.kesden.b250.groupfour.group4padsim;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void newGame(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
