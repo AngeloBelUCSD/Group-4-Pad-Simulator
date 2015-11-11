8ff17e571fbd314795a0ed24620743dbb032ad66
vaiolis <harrisonllin@gmail.com>
2015-11-11 14:12:39 -0800

    Created OrbView Model

---
+++ src/main/java/com/kesden/b250/groupfour/group4padsim/OrbView.java
@@ -0,0 +1,58 @@
+package com.kesden.b250.groupfour.group4padsim;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class OrbView extends ImageView{
+
+    public static int RED_ORB = 101;
+    public static int BLUE_ORB = 201;
+    public static int GREEN_ORB = 301;
+    public static int LIGHT_ORB = 401;
+    public static int DARK_ORB = 501;
+    public static int HEAL_ORB = 601;
+
+    private int id;
+
+    /**
+     * @param context
+     */
+    public OrbView(Context context)
+    {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param context
+     * @param attrs
+     */
+    public OrbView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param context
+     * @param attrs
+     * @param defStyle
+     */
+    public OrbView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+        // TODO Auto-generated constructor stub
+    }
+
+    public void setID(int id)
+    {
+        this.id = id;
+    }
+
+    public int getID()
+    {
+        return id;
+    }
+}
