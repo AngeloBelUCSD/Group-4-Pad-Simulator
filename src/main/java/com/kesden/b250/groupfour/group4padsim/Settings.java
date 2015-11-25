6ba05d90cdaff0d41cb0502c81abc5bc904db156
Spencer Huang <syhuang@ucsd.edu>
2015-11-25 05:20:25 -0800

    Settings and HighScoreActivity no longer rotate. Removed "Resume Game" button from MainMenu. Changed application name to PADSIM. Changed application icon. Added baseline setting functionality. Added setting and high score to shared preferences.

--- src/main/java/com/kesden/b250/groupfour/group4padsim/Settings.java
+++ src/main/java/com/kesden/b250/groupfour/group4padsim/Settings.java
@@ -1,20 +1,33 @@
 package com.kesden.b250.groupfour.group4padsim;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends AppCompatPreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.game_preferences);
 
+        Preference reset_hs_button = findPreference(getString(R.string.reset_hs_key));
 
+        reset_hs_button.setOnPreferenceClickListener(
+                new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+
+                        return true;
+                    }
+                }
+        );
     }
+
+
 
 }
