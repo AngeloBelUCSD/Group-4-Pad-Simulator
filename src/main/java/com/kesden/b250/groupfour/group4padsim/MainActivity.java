da54ca9fc9a585a8f1c046b55b26bcc6ccee1b2b
vaiolis <harrisonllin@gmail.com>
2015-12-01 23:40:59 -0800

    Return to MainMenu on Game Over

--- src/main/java/com/kesden/b250/groupfour/group4padsim/MainActivity.java
+++ src/main/java/com/kesden/b250/groupfour/group4padsim/MainActivity.java
@@ -253,9 +253,15 @@ public class MainActivity extends AppCompatActivity implements OnDragListener, O
                     draggedOrb.setVisibility(View.INVISIBLE);
                     Log.d(TAG, "Orb ID is " + draggedOrb.getID());
                     return true; // if you want to handle the touch event
+                } else {
+                    finish();
                 }
             case MotionEvent.ACTION_UP:
                 // RELEASED
+                if (manager.isGameOver())
+                {
+                    finish();
+                }
                 return true; // if you want to handle the touch event
         }
         return false;
